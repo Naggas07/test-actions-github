03a1383e0b279916c87f786af0976550abd82ac8
Naggas07 <rober.araya.gil@gmail.com>
2021-03-30 19:13:22 +0200

    7 step

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.listen(5000, () => console.log("listen on port 5000"));
