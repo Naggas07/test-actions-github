f5b9bbfdd4a54d2f8b5261672a7de4299eb9d69e
Naggas07 <rober.araya.gil@gmail.com>
2021-03-30 20:55:21 +0200

    test41

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
+
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(5000, () => console.log("listen on port 5000"));
+app.listen(port, () => console.log("listen on port " + port));
